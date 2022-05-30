3496d95cf7b27fda11bcba068c0da72ed812554c
akinbodebams <secretariat@abc.org.ng>
2022-05-30 23:34:00 +0100

    time for back end

--- static/js/script.js
+++ static/js/script.js
@@ -4,6 +4,14 @@ const add_more_occupants = document.querySelector(".add_more_occupants");
 const proceed_dependants = document.querySelector(".proceed_dependants");
 const section_1 = document.querySelector(".Section_1");
 const section_2 = document.querySelector(".section_2");
+const section_3 = document.querySelector(".Section_3");
+const section_4 = document.querySelector(".Section_4");
+const section_5 = document.querySelector(".section_5");
+const proceed_employment = document.querySelector(".proceed_employment");
+const proceed_credit = document.querySelector(".proceed_credit");
+const proceed_submit = document.querySelector(".proceed_submit");
+
+console.log(section_5);
 
 const html = `
  <div class="row row-space counter occupants_details" >
@@ -20,12 +28,6 @@ const html = `
 </div>
 
      `;
-
-proceed_dependants.addEventListener("click", function (e) {
-  e.preventDefault();
-  console.log(section_2);
-  section_1.style.display = "none";
-});
 
 add_occupant.addEventListener("click", function (e) {
   occupants_details.style.display = "";
@@ -45,4 +47,24 @@ add_more_occupants.addEventListener("click", function (e) {
   }
   //     console.log(all_occupants.length + 1);
   occupants_details.insertAdjacentHTML("afterend", html);
+});
+
+proceed_dependants.addEventListener("click", function (e) {
+  section_1.style.display = "none";
+  section_2.style.display = "block";
+});
+
+proceed_employment.addEventListener("click", (e) => {
+  section_2.style.display = "none";
+  section_3.style.display = "block";
+});
+
+proceed_credit.addEventListener("click", (e) => {
+  section_3.style.display = "none";
+  section_4.style.display = "block";
+});
+
+proceed_submit.addEventListener("click", (e) => {
+  section_4.style.display = "none";
+  section_5.style.display = "block";
 });
