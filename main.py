b3736195bc6dd754ebdbd13fafe778afbe6f9ca6
akinbodebams <secretariat@abc.org.ng>
2022-05-31 15:34:36 +0100

    tweaked

--- main.py
+++ main.py
@@ -77,70 +77,9 @@ def new_member():
 def thank_u():
     return render_template('thanku.html')
 
-# def member_list():
-#     if request.method == 'POST':
-#         file = request.files.get('profile_picture')
-#         hashed_pw = generate_password_hash(request.form['password'], "sha256")
-#         new_member = Members(
-#             first_name=request.form['first_name'],
-#             middle_name=request.form['middle_name'],
-#             surname=request.form['surname'],
-#             gender=request.form['gender'],
-#             marital_status=request.form['marital_status'],
-#             maiden_name=request.form['maiden_name'],
-#             phone_number=request.form['phone_number'],
-#             second_phone_number=request.form['second_phone_number'],
-#             email=request.form['email'],
-#             residential_address=request.form['residential_address'],
-#             state_of_origin=request.form['state_of_origin'],
-#             state_lga=request.form['state_lga'],
-#             permanent_address=request.form['permanent_address'],
-#             occupation=request.form['occupation'],
-#             baptism=request.form['baptism'],
-#             society=request.form['society'],
-#             date_of_birth=request.form['date_of_birth'],
-#             wedding_date=request.form['wedding_date'],
-#             password_hash=hashed_pw,
-#             profile_picture=save_image(file, request.form['first_name'],request.form['surname']))
-#         lst = ['position', 'start_date', 'end_date', 'department']
-#         new = []
-#         for i in lst:
-#             f = request.form.getlist(i)
-#             new.append(f)
-#         zipping_lst_new = zip(lst, new)
-#         zipped_position_key_value = dict(zipping_lst_new)
-#         position_department = zipped_position_key_value['department']
-#         position_end_dates = zipped_position_key_value['end_date']
-#         position_start_dates = zipped_position_key_value['start_date']
-#         position_positions = zipped_position_key_value['position']
-#         lenght_of_values = len(position_department)
-#
-#         counter = 0
-#         for i in range(lenght_of_values):
-#             new_position = Position(position=position_positions[counter],
-#                                     department=position_department[counter],
-#                                     end_date=position_end_dates[counter],
-#                                     start_date=position_start_dates[counter])
-#             new_member.positions.append(new_position)
-#             counter += 1
-#
-#         db.session.add(new_member)
-#         db.session.commit()
-#         flash('Member Successfully Added')
-#         return redirect(url_for('member_list'))
-#
-#     return render_template('member/member_list.html', state_list=states,
-#                            gender_list=gender, society_list=society, departments_list=departments,
-#                            member=Members.query.all())
-#
-#
-# @app.route('/new_member', methods=["GET", "POST"])
-# def new_member():
-#     return render_template('member/add_members.html', state_list=states, gender_list=gender, society_list=society,
-#                            departments_list=departments)
+
 #
 
 
-
-
-app.run(debug=True)
+if __name__=='__main__':
+    app.run(debug=True)
